282767b0554badb477ed12ff1e083eebfca32a82
RupendraCHA <chandaluri210@gmail.com>
2024-12-16 16:29:30 +0530

    Creating new Model

--- backend/models/odataModel.js
+++ backend/models/odataModel.js
@@ -3,14 +3,15 @@ import mongoose from 'mongoose'
 const automationSchema = new mongoose.Schema({
     Process: String,
     Automation_to_Hanlytic_np : [{
+        _id: false,
         Product_ID: String,
         Product_name: String,
         Distribution_Center: String,
         Quantity: String
     }
 ]
-},
-{ _id: false })
+})
+
 
 const AutomationData = mongoose.model('AutomationData', automationSchema);
 
