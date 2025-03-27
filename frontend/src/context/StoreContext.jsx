07bc5a060bc79e304831c0d467f3fa840501b457
RupendraCHA <chandaluri210@gmail.com>
2025-03-28 00:26:26 +0530

    Deployed Application on render

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     // let url = "http://localhost:3001" // Local
-    let url = "https://hanelytics-solution-visionsoft.onrender.com" // origin
+    // let url = "https://hanelytics-solution-visionsoft.onrender.com" // origin
+    let url = "https://han-elytics-solution-visionsoft-pej2.vercel.app" // origin
 
     const [token, setToken] = useState("")
     const [username, setUsername] = useState("")
