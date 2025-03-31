4173a0c8b18434c2a89ca1fa03cc6d2f023aa7b8
RupendraCHA <chandaluri210@gmail.com>
2025-03-31 15:31:48 +0530

    Changing URL Addresses

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    // origin: ["https://hanelytics-solution-visionsoft-1.onrender.com"], // origin
+    // origin: ["http://localhost:5173"],
+    origin: ["https://hanelytics-solution-visionsoft-1.onrender.com"], // origin
     // origin: ["https://han-elytics-solution-visionsoft.vercel.app"], // origin
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
