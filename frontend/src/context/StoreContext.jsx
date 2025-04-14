eeb6c35ed895830b7edbdd22047ff8c616a4592c
Ramarani25 <ramarani.battu@gmail.com>
2025-04-14 18:48:28 +0530

    Updating Production Planning Dashboard Url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -16,24 +16,24 @@ const StoreContextProvider = (props) => {
     const [accessNameInput, setAccessNameInput] = useState("")
     const [allDashboards, setAllDashboards] = useState([])
     const [dashUpdateId, setDashUpdateId] = useState("")
-  const [storeUserDashboardData, setStoreUserDashboardData] = useState([])
+    const [storeUserDashboardData, setStoreUserDashboardData] = useState([])
 
     const [sapSalesModuleText, setSapSalesModuleText] = useState(false);
-  const [sapMaterialsModuleText, setSapMaterialsModuleText] = useState(false);
-  const [sapIntroText, setSapIntroText] = useState(true);
-  const [menu, setMenu] = useState("home");
-  const [isLoading1, setLoading1] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-  const [salesTable, setSalesTable] = useState("");
-  const [homeText, setHomeText] = useState(true);
-  const [homeText1, setHomeText1] = useState(true);
-  const [loadProcurementData, setLoadProcurementData] = useState(false);
-  const [loadSalesData, setLoadSalesData] = useState(false);
-  const [loadOTC, setLoadOTC] = useState(false);
-  const [loadProcurement, setLoadProcurement] = useState(false);
+    const [sapMaterialsModuleText, setSapMaterialsModuleText] = useState(false);
+    const [sapIntroText, setSapIntroText] = useState(true);
+    const [menu, setMenu] = useState("home");
+    const [isLoading1, setLoading1] = useState(false);
+    const [isLoading, setLoading] = useState(false);
+    const [salesTable, setSalesTable] = useState("");
+    const [homeText, setHomeText] = useState(true);
+    const [homeText1, setHomeText1] = useState(true);
+    const [loadProcurementData, setLoadProcurementData] = useState(false);
+    const [loadSalesData, setLoadSalesData] = useState(false);
+    const [loadOTC, setLoadOTC] = useState(false);
+    const [loadProcurement, setLoadProcurement] = useState(false);
 
-  const fromDate = "2023-08-19";
-  const lastDate = "2025-01-13";
+    const fromDate = "2023-08-19";
+    const lastDate = "2025-01-13";
 
 
     const getAllUsersList = async () => {
@@ -42,7 +42,7 @@ const StoreContextProvider = (props) => {
         // console.log(users.data)
     }
 
-   
+
     useEffect(() => {
         const jwtToken = localStorage.getItem("token")
         const role = localStorage.getItem("role")
@@ -68,41 +68,41 @@ const StoreContextProvider = (props) => {
         setUsersList,
         accessNameInput,
         setAccessNameInput,
-        allDashboards, 
+        allDashboards,
         setAllDashboards,
         dashUpdateId,
         setDashUpdateId,
         storeUserDashboardData,
         setStoreUserDashboardData,
 
-    sapSalesModuleText,
-    setSapSalesModuleText,
-    fromDate,
-    lastDate,
-    sapMaterialsModuleText,
-    setSapMaterialsModuleText,
-    sapIntroText,
-    setSapIntroText,
-    menu,
-    setMenu,
-    isLoading1,
-    setLoading1,
-    isLoading,
-    setLoading,
-    salesTable,
-    setSalesTable,
-    homeText,
-    setHomeText,
-    homeText1,
-    setHomeText1,
-    loadProcurementData,
-    setLoadProcurementData,
-    loadSalesData,
-    setLoadSalesData,
-    loadOTC,
-    setLoadOTC,
-    loadProcurement,
-    setLoadProcurement,
+        sapSalesModuleText,
+        setSapSalesModuleText,
+        fromDate,
+        lastDate,
+        sapMaterialsModuleText,
+        setSapMaterialsModuleText,
+        sapIntroText,
+        setSapIntroText,
+        menu,
+        setMenu,
+        isLoading1,
+        setLoading1,
+        isLoading,
+        setLoading,
+        salesTable,
+        setSalesTable,
+        homeText,
+        setHomeText,
+        homeText1,
+        setHomeText1,
+        loadProcurementData,
+        setLoadProcurementData,
+        loadSalesData,
+        setLoadSalesData,
+        loadOTC,
+        setLoadOTC,
+        loadProcurement,
+        setLoadProcurement,
     }
     return (
         <StoreContext.Provider value={contextValue}>
