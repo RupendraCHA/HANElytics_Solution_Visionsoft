27d81cb974dc77dab8d6b1bbedbdfb62e1eddc4c
Ramarani25 <ramarani.battu@gmail.com>
2025-04-17 16:32:06 +0530

    styling website

--- frontend/src/pages/InitialPage/InitialPage.jsx
+++ frontend/src/pages/InitialPage/InitialPage.jsx
@@ -93,72 +93,72 @@ function InitialPage() {
       title: "1. Inventory Reorder Point & Safety Stock Prediction - With and Without Live Data",
       description:
         "Predict optimal reorder points and safety stock levels using AI/ML to prevent stockouts or overstock situations—improving supply chain efficiency.",
-        image: `${assets.Inventory_model}`
+      image: `${assets.Inventory_model}`
     },
     {
-        modelId: 2,
+      modelId: 2,
       title: "2. Revenue & Demand Sensing for Pharmaceuticals",
       description:
         "Forecasts revenue generation based on market behavior, seasonal trends, and historical sales data to get insight on which category giving more profits.",
-        image: `${assets.Clinical_model}`
+      image: `${assets.Clinical_model}`
     },
     {
-        modelId: 3,
+      modelId: 3,
       title: "3. Medical Equipment Risk Detection & Failure Prevention",
       description:
         "Predicted results are displayed in a clean, structured table format, showcasing each key metric individually for easy review and interpretation.",
-        image: `${assets.Equipment_model}`
+      image: `${assets.Equipment_model}`
     },
     {
-        modelId: 4,
+      modelId: 4,
       title: "4. Reorder Point & Safety Stock Predictions for Clinical Drugs/Medicine",
       description:
         "Ensure availability of critical clinical medicines by forecasting demand and recommending adequate buffer stock levels without interrupting the patient care.",
-        image: `${assets.Revenue_model}`
+      image: `${assets.Revenue_model}`
     },
-    
+
   ];
 
   const capabilities = [
     {
-        id: 1,
-        text: "Multi-Model Integration"
+      id: 1,
+      text: "Multi-Model Integration"
     },
     {
       id: 2,
       text: "User-Centric Design"
-  },
+    },
     {
       id: 3,
       text: "Data Transparency & Drill-Down"
-  },
-    {
-        id: 4,
-        text: "Enterprise Scalability"
     },
     {
-        id: 5,
-        text: "Role-based access (Admin/User) for controlled experience"
+      id: 4,
+      text: "Enterprise Scalability"
     },
     {
-        id: 6,
-        text: "No-Code Visualization Integration"
+      id: 5,
+      text: "Role-based access (Admin/User) for controlled experience"
     },
     {
-        id: 7,
-        text: "Unified view of datasets, predictions, graphs, and dashboards"
+      id: 6,
+      text: "No-Code Visualization Integration"
     },
     {
-        id: 8,
-        text: "Downloadable results for portability and analysis"
+      id: 7,
+      text: "Unified view of datasets, predictions, graphs, and dashboards"
     },
     {
-        id: 9,
-        text: "Quick Deployment & Easy Adoption"
+      id: 8,
+      text: "Downloadable results for portability and analysis"
     },
     {
-        id: 10,
-        text: "Many more in progress..."
+      id: 9,
+      text: "Quick Deployment & Easy Adoption"
+    },
+    {
+      id: 10,
+      text: "Many more in progress..."
     },
   ]
 
@@ -167,22 +167,22 @@ function InitialPage() {
       id: 1,
       text: "Smart Predictions, Simplified Interface",
       description: "Built for business and no technical knowledge needed."
-  },
+    },
     {
       id: 2,
       text: "Unified Data Platform",
       description: "Say goodbye to scattered tools. Hanelytics brings data, intelligence, and action under one roof."
-  },
+    },
     {
       id: 3,
       text: "Professional, Responsive Design",
       description: "Fast, modern interface that works seamlessly across desktop and mobile devices."
-  },
+    },
     {
       id: 4,
       text: "Secure & Scalable",
       description: "Built on secure architecture with performance and scalability in mind"
-  },
+    },
   ]
 
   const faqs = [
@@ -190,39 +190,39 @@ function InitialPage() {
       id: 1,
       question: "What is HANElytics?",
       answer: "HANElytics is a unified analytics platform that leverages AI/ML models and interactive dashboards to help businesses make data-driven decisions. It simplifies forecasting, inventory planning, risk analysis, and demand sensing through an intuitive and user-friendly interface."
-  },
+    },
     {
       id: 2,
       question: " Who can use HANElytics?",
       answer: "HANElytics is designed for business analysts, supply chain managers, healthcare professionals, and anyone who needs predictive insights without diving deep into technical modeling."
-  },
+    },
     {
       id: 3,
       question: "Do I need to install Power BI to view dashboards?",
       answer: "No. Hanelytics comes with embedded Power BI dashboards."
-  },
+    },
     {
       id: 4,
       question: "Can I download the prediction results?",
       answer: "Yes. You can export model insights and data in Excel format with just one click. This feature is useful for reports, meetings, or offline analysis."
-  },
+    },
     {
       id: 5,
       question: "Is my data secure on HANElytics?",
       answer: "Absolutely. HANElytics is built with secure authentication, role-based access, and encrypted data handling to ensure your information stays protected."
-  },
+    },
     {
       id: 6,
       question: "Can I try the platform before signing up?",
       answer: "Yes you can request for demo using contact details in the footer section."
       // A live demo mode or preview is under development to allow users to explore basic features before registering."
       // You can also request a demo session via the contact page.
-  },
+    },
     {
       id: 7,
       question: "What if I forget my login credentials?",
       answer: "The login page includes a password recovery option. Just click Forgot Password and follow the instructions to reset your credentials securely."
-  },
+    },
   ]
 
   return (
@@ -239,24 +239,24 @@ function InitialPage() {
                 className="image-size"
               /> */}
               <Link to="/home" className="initial-page-home-heading">
-                        <h1 className="header-home-text">HANELYTICS</h1>
+                <h1 className="header-home-text">HANELYTICS</h1>
               </Link>
             </div>
             <div className="initial-page-tab-buttons">
               <a href="#keyFeatures"  >
-                <p className={`${activeSection === "keyFeatures" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("keyFeatures")}>Key Features</p>
+                <p className={`${activeSection === "keyFeatures" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("keyFeatures")}>Key Features</p>
               </a>
               <a href="#modelsOverview">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("modelsOverview")}>Models Overview</p>
+                <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("modelsOverview")}>Models Overview</p>
               </a>
               <a href="#capabilities">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("capabilities")}>Capabilities</p>
+                <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("capabilities")}>Capabilities</p>
               </a>
               <a href="#whyChooseHanelytics">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("whyChooseHanelytics")}>Why Choose HANElytics?</p>
+                <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("whyChooseHanelytics")}>Why Choose HANElytics?</p>
               </a>
               <a href="#faqs">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("faqs")}>FAQs</p>
+                <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("faqs")}>FAQs</p>
               </a>
             </div>
             <div className="login-register-buttons">
@@ -295,20 +295,20 @@ function InitialPage() {
                 <div className="initial-page-mobile-menu">
                   {/* <p>Key Features</p> */}
                   <a href="#keyFeatures"  >
-                <p className={`${activeSection === "keyFeatures" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("keyFeatures")}>Key Features</p>
-              </a>
-              <a href="#modelsOverview">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("modelsOverview")}>Models Overview</p>
-              </a>
-              <a href="#capabilities">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("capabilities")}>Capabilities</p>
-              </a>
-              <a href="#whyChooseHanelytics">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("whyChooseHanelytics")}>Why Choose HANElytics?</p>
-              </a>
-              <a href="#faqs">
-                <p className={`${activeSection === "modelsOverview" ? "text-underline": "feature-section"}`} onClick={() => setActiveSectionText("faqs")}>FAQs</p>
-              </a>
+                    <p className={`${activeSection === "keyFeatures" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("keyFeatures")}>Key Features</p>
+                  </a>
+                  <a href="#modelsOverview">
+                    <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("modelsOverview")}>Models Overview</p>
+                  </a>
+                  <a href="#capabilities">
+                    <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("capabilities")}>Capabilities</p>
+                  </a>
+                  <a href="#whyChooseHanelytics">
+                    <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("whyChooseHanelytics")}>Why Choose HANElytics?</p>
+                  </a>
+                  <a href="#faqs">
+                    <p className={`${activeSection === "modelsOverview" ? "text-underline" : "feature-section"}`} onClick={() => setActiveSectionText("faqs")}>FAQs</p>
+                  </a>
                   <Link
                     to="/register"
                     className="bg-warning rounded-2 text-white mobile-button"
@@ -342,7 +342,7 @@ function InitialPage() {
               {keyFeatures.map((feature) => (
                 <div key={feature.featureId} className="feature-card">
                   <h3>
-                    <FaHandPointRight style={{marginRight: "5px"}}/>
+                    <FaHandPointRight style={{ marginRight: "5px" }} />
                     {feature.title}</h3>
                   <p>{feature.description}</p>
                 </div>
@@ -356,12 +356,12 @@ function InitialPage() {
               {modelsOverview.map((model) => (
                 <div key={model.modelId} className="model-feature-card">
                   <div>
-                      <h3>
-                        {model.title}
-                        </h3>
-                      <p>{model.description}</p>
+                    <h3>
+                      {model.title}
+                    </h3>
+                    <p>{model.description}</p>
                   </div>
-                  <a download href={model.image}><img src={model.image}/></a>
+                  <a download href={model.image}><img src={model.image} /></a>
                 </div>
               ))}
             </div>
@@ -372,10 +372,10 @@ function InitialPage() {
               {capabilities.map((item) => (
                 <div key={item.id} className="feature-card">
                   <div>
-                      <h3 style={{fontSize: "13px", textAlign: 'left'}}>
-                        <FaHandPointRight style={{marginRight: "5px"}}/>
-                        {item.text}
-                        </h3>
+                    <h3 style={{ fontSize: "13px", textAlign: 'left' }}>
+                      <FaHandPointRight style={{ marginRight: "5px" }} />
+                      {item.text}
+                    </h3>
                   </div>
                 </div>
               ))}
@@ -387,30 +387,30 @@ function InitialPage() {
               {whyChooseHANElytics.map((item) => (
                 <div key={item.id} className="feature-card">
                   <div>
-                      <h3>
-                        <FaHandPointRight style={{marginRight: "5px"}}/>
-                        {item.text}
-                        </h3>
-                        <p>{item.description}</p>
+                    <h3>
+                      <FaHandPointRight style={{ marginRight: "5px" }} />
+                      {item.text}
+                    </h3>
+                    <p>{item.description}</p>
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <div className="container key-features-section" style={{marginBottom: "0px"}} id="faqs">
+          <div className="container key-features-section" style={{ marginBottom: "0px" }} id="faqs">
             <h1 className="">FAQs:</h1>
             <div className="faqs-features-items-container">
               {faqs.map((item) => (
                 <div key={item.id} className="feature-card">
                   <div>
-                      <div className="question-answer" onClick={() => viewAnswerOnClick(item.id)}>
-                        <h3 style={{fontSize: "14px"}}>
-                          <FaHandPointRight style={{marginRight: "5px"}}/>
-                          {item.question}
-                          </h3>
-                          <IoIosArrowDown/>
-                      </div>
-                      {item.id === answerId && <p>{item.answer}</p>}
+                    <div className="question-answer" onClick={() => viewAnswerOnClick(item.id)}>
+                      <h3 style={{ fontSize: "14px" }}>
+                        <FaHandPointRight style={{ marginRight: "5px" }} />
+                        {item.question}
+                      </h3>
+                      <IoIosArrowDown />
+                    </div>
+                    {item.id === answerId && <p>{item.answer}</p>}
                   </div>
                 </div>
               ))}
