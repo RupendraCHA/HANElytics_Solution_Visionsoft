b26c26b42118ca379ca4ab8330d4ff9427d79bb6
RupendraCHA <chandaluri210@gmail.com>
2025-04-25 15:57:37 +0530

    Developing Session expiry functionality for HANElytics Website

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -122,7 +122,7 @@ function Login() {
     if (response.data.success) {
       setExist(false);
       setToken(response.data.token);
-      const expiresIn = 360 * 60 * 1000; // 10 min expiry
+      const expiresIn = 60 * 60 * 1000; // One hour expairy
       const expiryTime = Date.now() + expiresIn;
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("username", response.data.name);
